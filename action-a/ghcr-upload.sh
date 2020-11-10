0779d22f38b020b89d62811b219ae287b2fd7ab1
Daniel Freithofer <daniel.freithofer@trigo.at>
2020-11-10 13:55:17 +0100

    making ghcr-upload.sh executable

--- action-a/ghcr-upload.sh
+++ action-a/ghcr-upload.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 echo ${{ secrets.TRIGO_PACKAGE_ACCESS_TOKEN }} | docker login ghcr.io -u $GITHUB_ACTOR --password-stdin
 docker build -t ghcr.io/trigo-at/docker-json-server:latest - < Dockerfile
