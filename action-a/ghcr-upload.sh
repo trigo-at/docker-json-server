16e070a872c753753081414e7275743019bad11f
Daniel Freithofer <daniel.freithofer@trigo.at>
2020-11-11 14:32:13 +0100

    discovering correct way for actions

--- action-a/ghcr-upload.sh
+++ action-a/ghcr-upload.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
 echo $TRIGO_PACKAGE_ACCESS_TOKEN | docker login ghcr.io -u $GITHUB_ACTOR --password-stdin
-docker build . -t trigo-at/docker-json-server
-cat run.sh
-docker tag  trigo-at/docker-json-server ghcr.io/trigo-at/docker-json-server:latest
+docker build . --tag ghcr.io/trigo-at/docker-json-server:latest
 docker push ghcr.io/trigo-at/docker-json-server:latest
