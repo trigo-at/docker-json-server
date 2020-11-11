5c076901f45b3c9848d0de16cf4adb3c972a52e3
Daniel Freithofer <daniel.freithofer@trigo.at>
2020-11-11 14:00:43 +0100

    fixing docker build

--- action-a/ghcr-upload.sh
+++ action-a/ghcr-upload.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 echo $TRIGO_PACKAGE_ACCESS_TOKEN | docker login ghcr.io -u $GITHUB_ACTOR --password-stdin
-docker build https://github.com/trigo-at/docker-json-server#master -t trigo-at/docker-json-server
+docker build https://raw.githubusercontent.com/trigo-at/docker-json-server/master/Dockerfile -t trigo-at/docker-json-server
 docker tag  trigo-at/docker-json-server ghcr.io/trigo-at/docker-json-server:latest
 docker push ghcr.io/trigo-at/docker-json-server:latest
