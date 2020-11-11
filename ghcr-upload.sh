3e9ced741772ab415c7f95841408cfa484f56cce
danielfreithofer <71336043+danielfreithofer@users.noreply.github.com>
2020-11-11 13:17:20 +0100

    Delete ghcr-upload.sh

--- ghcr-upload.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo ${{ secrets.TRIGO_PACKAGE_ACCESS_TOKEN }} #| docker login ghcr.io -u $GITHUB_ACTOR --password-stdin
-#docker build -t ghcr.io/trigo-at/docker-json-server:latest - < Dockerfile
-#docker push ghcr.io/trigo-at/docker-json-server:latest
