800c470d5a7cbca9840b0a4dc46131587b78b20f
Daniel Freithofer <daniel.freithofer@trigo.at>
2020-11-12 11:59:43 +0100

    changing static path to variable

--- ghcr-upload.sh
+++ ghcr-upload.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-docker build . --tag ghcr.io/trigo-at/docker-json-server:${GITHUB_REF##*/}
+docker build . --tag ghcr.io/$GITHUB_REPOSITORY:${GITHUB_REF##*/}
 echo $TRIGO_PACKAGE_ACCESS_TOKEN | docker login ghcr.io -u $GITHUB_ACTOR --password-stdin
-docker push ghcr.io/trigo-at/docker-json-server:${GITHUB_REF##*/}
+docker push ghcr.io/$GITHUB_REPOSITORY:${GITHUB_REF##*/}
